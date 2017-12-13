1500c477ffc0bba34bca960583dcdd9b9166cc23
elvinos <aep.charles@gmail.com>
2017-12-13 11:43:06 +0000

    Added Projects Page

--- site/web/app/themes/ixnTheme/resources/views/template-projects.blade.php
+++ site/web/app/themes/ixnTheme/resources/views/template-projects.blade.php
@@ -8,6 +8,26 @@
 
     @include('partials.page-header')
 
+    <form action="<?php echo site_url() ?>/wp-admin/admin-ajax.php" method="POST" id="filter">
+	    <?php
+	    if( $terms = get_terms( 'category', 'orderby=name' ) ) : // to make it simple I use default categories
+		    echo '<select name="categoryfilter"><option>Select category...</option>';
+		    foreach ( $terms as $term ) :
+			    echo '<option value="' . $term->term_id . '">' . $term->name . '</option>'; // ID of the category as the value of an option
+		    endforeach;
+		    echo '</select>';
+	    endif;
+	    ?>
+        <label>
+            <input type="radio" name="date" value="ASC" /> Date: Ascending
+        </label>
+        <label>
+            <input type="radio" name="date" value="DESC" selected="selected" /> Date: Descending
+        </label>
+        <button>Apply filter</button>
+        <input type="hidden" name="action" value="myFilter">
+    </form>
+    <div id="response"></div>
     <?php
 
     $vargsposts = array(
@@ -73,4 +93,31 @@
         </div>
 
     </section>
+    <script>
+    jQuery(function($){
+    $('#filter').submit(function(){
+    var filter = $('#filter');
+    $.ajax({
+    url:filter.attr('action'),
+    data:filter.serialize(), // form data
+    type:filter.attr('method'), // POST
+    beforeSend:function(xhr){
+    filter.find('button').text('Processing...'); // changing the button label
+    },
+    success:function(data){
+    filter.find('button').text('Apply filter'); // changing the button label back
+        if (data.status === 'success') {
+    $('#response').html(data); // insert data
+            }
+            else{
+            filter.find('button').text('broken...');
+        }
+
+    }
+    });
+    return false;
+    });
+    });
+
+    </script>
 @endsection
