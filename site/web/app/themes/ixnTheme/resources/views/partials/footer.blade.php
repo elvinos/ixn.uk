8e33f180a846d9b89d57d05059dc7b003a6b9828
elvinos <aep.charles@gmail.com>
2017-12-28 11:58:16 +0000

    Completed events and other tweaks finalising footer

--- site/web/app/themes/ixnTheme/resources/views/partials/footer.blade.php
+++ site/web/app/themes/ixnTheme/resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="content-info" id="footerSec">
- <div class="topcontainer col-lg-12">
+ <div class="topcontainer">
     </div>
 <div class="container-fluid footer">
   
@@ -8,21 +8,24 @@
           <div class="row" id="logoRow">
               <div class="col-4 my-auto logo" id="ixnLogo">
               </div>
-              <div class="col-4 logo"  id="uclEngLogo">
+              <div class="col-4 my-auto logo" id="uclEngLogo">
               </div>
-              <div class="col-4 logo"  id="uclLogo">
+              <div class="col-4 my-auto logo" id="uclLogo">
               </div>
             </div>
         </div>
-        <div class="col space">
+        <div class="colspace"></div>
+        <div class="addressBlock">
+            <div class="row" id="logoRow">
+                <div class="col-md-12 mx-auto" id="addressText">
+                    <a href="mailto:y.fu@cs.ucl.ac.uk" class="fa fa-envelope" id="fa-footer"></a>
+                  <a href="tel:++4402031087129" class="fa fa-phone" id="fa-footer"></a>
+                  <a href="https://www.google.co.uk/maps/place/The+UCL:+Department+of+Computer+Science/@51.523188,-0.1346016,17z/data=!3m1!4b1!4m5!3m4!1s0x48761b2f06cd24d9:0x921db3ae5ef57ce7!8m2!3d51.523188!4d-0.1324129" class="fa fa-location-arrow" id="fa-footer"></a>
+                  <p id="footerPage">66-72 Gower Street,<br>
+                    WC1E 6BT,<br>London, <br>United Kingdom</p>
+                 </div>
+            </div>
         </div>
-        <div class="address"> <div class="col-md-12 mx-auto" id="address">
-       <a href="mailto:y.fu@cs.ucl.ac.uk" class="fa fa-envelope" id="fa-footer"></a>
-      <a href="tel:++4402031087129" class="fa fa-phone" id="fa-footer"></a>
-      <a href="https://www.google.co.uk/maps/place/The+UCL:+Department+of+Computer+Science/@51.523188,-0.1346016,17z/data=!3m1!4b1!4m5!3m4!1s0x48761b2f06cd24d9:0x921db3ae5ef57ce7!8m2!3d51.523188!4d-0.1324129" class="fa fa-location-arrow" id="fa-footer"></a>
-      <p id="footerPage">66-72 Gower Street,<br>
-        WC1E 6BT,<br>London, <br>United Kingdom</p>
-    </div></div>
     </div>
 </div>
 </div>
